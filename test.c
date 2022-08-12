2130fb9eb7bc3b3001d0ce187109be88306bc147
fadal ba <fadalba@gmail.com>
2022-08-12 10:05:00 +0000

    vérification d'un nombre positif

---
+++ test.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+ 
+int main()
+{
+   int a;
+   
+   printf("Entrez un entier\n");
+   scanf("%d", &a);
+
+if (a>0)
+{
+    printf("la valeur de a est positif"); 
+}
+else {
+    printf("la valeur saisie est négatif");
+    }
+
+ }
